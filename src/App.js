85ea04abfe9227dda89ed15a31a6f3fa72b95f5e
Mahmut Yiğit <mahmutyigit96@hotmail.com>
2021-01-10 00:44:28 +0300

    ui components added.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography, Grid, TextField } from "@material-ui/core";
+
+const ForceGraphy = () => {
+  return <Typography></Typography>;
+};
+
+const MassField = () => {
+  return <TextField variant="outlined" />;
+};
+
+const AccelerationField = () => {
+  return <TextField variant="outlined" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      // style={{ height: "%100", background: "#000000" }}
+      container
+      direction="row"
+      justify="center"
+      spacing={10}
+    >
+      <Grid item>
+        <Grid container direction="column">
+          <Grid item>
+            <Typography>F</Typography>
+          </Grid>
+          <Grid item>
+            <ForceGraphy />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid container direction="column">
+          <Grid item>
+            <Typography>=</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid container direction="column" alignItems="center">
+          <Grid item style={{ width: "%100" }}>
+            <Typography>M</Typography>
+          </Grid>
+          <Grid item>
+            <MassField />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Grid container direction="column" alignItems="center">
+          <Grid item style={{ width: "%100" }}>
+            <Typography>A</Typography>
+          </Grid>
+          <Grid item>
+            <AccelerationField />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
